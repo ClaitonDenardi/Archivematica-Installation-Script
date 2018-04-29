06f72b367aa30e3719ff5f69df74d6bf5250ce84
Claiton Denardi <37638849+ClaitonDenardi@users.noreply.github.com>
2018-04-29 10:07:04 -0300

    Add files via upload

---
+++ Instalar_Archivematica_14.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Script de Instalação Archivematica
+# Claiton Denardi Paulus
+# BaseGED_2018/1
+
+#Atualizar o repositorio
+sudo apt-get update
+#Python
+sudo apt-get install python-software-properties
+#Adicionar o archivematica
+sudo add-apt-repository ppa:archivematica/1.4
+sudo wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo sh -c 'echo "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main" >> /etc/apt/sources.list'
+sudo apt-get update
+sudo apt-get upgrade
+#Instalação do Archivematica
+sudo apt-get install archivematica-storage-service
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/storage /etc/nginx/sites-enabled/storage
+sudo ln -s /etc/uwsgi/apps-available/storage.ini /etc/uwsgi/apps-enabled/storage.ini
+sudo service uwsgi restart
+#Reinicia-se o nginx para setar as novas configs
+sudo service nginx restart
+sudo apt-get install archivematica-mcp-server
+sudo apt-get install archivematica-mcp-client
+sudo apt-get install archivematica-dashboard
+#Instalando o servidor de buscas ElasticSearch
+sudo apt-get install elasticsearch
+sudo rm -f /etc/apache2/sites-enabled/*default*
+sudo wget -q https://raw.githubusercontent.com/artefactual/archivematica/stable/1.4.x/localDevSetup/apache/apache.default -O /etc/apache2/sites-available/default.conf
+sudo ln -s /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf
+sudo /etc/init.d/apache2 restart
+sudo freshclam
+sudo /etc/init.d/clamav-daemon start
+sudo /etc/init.d/elasticsearch restart
+sudo /etc/init.d/gearman-job-server restart
+#Iniciando Server
+sudo start archivematica-mcp-server
+#Iniciando Cliente
+sudo start archivematica-mcp-client
+sudo start fits
+sudo restart gearman-job-server
+echo "PRONTO, siga para o Browser..."
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Script-Instalacao-Archivematica
+
+# Script de Instalação do Archivematica 1.6.1 no Ubuntu 14.04
+
+### Instruções de instalação do Archivematica:
+* Faça o download do zip pelo GitHub;
+* Extraia os arquivos em um diretorio de seu PC;
+* No terminal acesse o diretorio em que voce extraiu os arquivos;
+* Como root no ubuntu execute o seguinte arquivo no terminal   ``script.sh``
+* Serão solicitadas informações durante a instalação, responda todas e aguarde.
+* Acesso ao Archivematica -> http://localhost/installer/welcome/
+* Acesso ao Archivematica Storage Service -> http://localhost:8000/
+* O usuario e senha padrões para login são ``User: test`` & ``Password: test``
+**Leia o guia de pós-instalação no site do archivematica para mais detalhes -> [post-install](https://www.archivematica.org/en/docs/archivematica-1.6/admin-manual/installation/installation/#post-install-config)** 
+
+### Importante!!!
+* É de extrema importância que o script seja executado em um Ubuntu "zerado", sem demais programas instalados, pois os mesmos podem conflitar com algum processo da instalação.
+
+![Archivematica](https://wiki.archivematica.org/images/a/a8/ArchivematicaTranslucent.png)   
